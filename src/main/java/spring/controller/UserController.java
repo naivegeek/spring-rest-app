020bad83213e1f5d404cf877c37d49fb2849a840
srinath <smedala@gmail.com>
2015-05-17 18:50:21 -0700

    formatting UserController

--- src/main/java/spring/controller/UserController.java
+++ src/main/java/spring/controller/UserController.java
@@ -17,7 +17,7 @@ import java.util.List;
  * Created by srinathmedala on 5/16/15.
  */
 
-@Api(value ="user",description = "Api related to User operations")
+@Api(value = "user", description = "Api related to User operations")
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -25,65 +25,64 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "login", method = RequestMethod.POST )
-    @ApiOperation(value ="Authenticate based on username and paswword ",httpMethod = "POST")
-    public @ResponseBody String login(@RequestBody AuthDto authDTO){
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    @ApiOperation(value = "Authenticate based on username and paswword ", httpMethod = "POST")
+    public @ResponseBody String login(@RequestBody AuthDto authDTO) {
         String auth = "failure";
-        if(authDTO!=null){
-           User user = userService.findUserByUsernameAndPassword(authDTO.getUsername(),authDTO.getPassword());
-           if(user!=null) {
-             auth ="success"  ;
-           }
+        if (authDTO != null) {
+            User user = userService.findUserByUsernameAndPassword(authDTO.getUsername(), authDTO.getPassword());
+            if (user != null) {
+                auth = "success";
+            }
         }
         return auth;
     }
 
-
-    @RequestMapping(value = "/all",method = RequestMethod.GET)
-    @ApiOperation(value ="Get List of all Users",httpMethod = "GET")
-    public @ResponseBody List<User> getAllUsers(){
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    @ApiOperation(value = "Get List of all Users", httpMethod = "GET")
+    public @ResponseBody List<User> getAllUsers() {
         return userService.findAllUsers();
     }
 
 
-    @RequestMapping(value = "/filter/city/{city}",method = RequestMethod.GET)
-    @ApiOperation(value ="Filter User By city name",httpMethod = "GET")
-    public @ResponseBody List<User> filterByCityName(@PathVariable("city") @ApiParam String cityName){
+    @RequestMapping(value = "/filter/city/{city}", method = RequestMethod.GET)
+    @ApiOperation(value = "Filter User By city name", httpMethod = "GET")
+    public @ResponseBody List<User> filterByCityName(@PathVariable("city") @ApiParam String cityName) {
         return userService.findUsersByCityName(cityName);
     }
 
-    @RequestMapping(value = "/filter/company/{company}",method = RequestMethod.GET)
-    @ApiOperation(value ="Filter User By company name",httpMethod = "GET")
-    public @ResponseBody List<User> filterByCompanyName(@PathVariable("company") @ApiParam String company){
+    @RequestMapping(value = "/filter/company/{company}", method = RequestMethod.GET)
+    @ApiOperation(value = "Filter User By company name", httpMethod = "GET")
+    public @ResponseBody List<User> filterByCompanyName(@PathVariable("company") @ApiParam String company) {
         return userService.findUsersByCompanyName(company);
     }
 
-    @RequestMapping(value ="/status",method = RequestMethod.GET)
-    @ApiOperation(value = "Check whether DB is Up",httpMethod ="GET")
-    public @ResponseBody String checkAppDependentComponentsAlive(){
+    @RequestMapping(value = "/status", method = RequestMethod.GET)
+    @ApiOperation(value = "Check whether DB is Up", httpMethod = "GET")
+    public @ResponseBody String checkAppDependentComponentsAlive() {
         String status = " DB is up";
-        String dbDown ="DB is down";
-        if(userService.isDbAlive()==1){
+        String dbDown = "DB is down";
+        if (userService.isDbAlive() == 1) {
             return status;
-        }else{
-           return dbDown;
+        } else {
+            return dbDown;
         }
     }
 
-    @RequestMapping(value ="/listFiles/{directoryName}",method = RequestMethod.GET)
-    @ApiOperation(value= "Lists all Files under given Directory",httpMethod = "GET")
-    public @ResponseBody List<String> listAllFiles(@PathVariable("directoryName") @ApiParam String directoryName){
+    @RequestMapping(value = "/listFiles/{directoryName}", method = RequestMethod.GET)
+    @ApiOperation(value = "Lists all Files under given Directory", httpMethod = "GET")
+    public @ResponseBody List<String> listAllFiles(@PathVariable("directoryName") @ApiParam String directoryName) {
 
         List<String> files = new ArrayList<String>();
         File directory = new File(directoryName);
-        if(directory!=null) {
+        if (directory != null) {
             File[] fList = directory.listFiles();
             for (File file : fList) {
-                if (file!=null && file.isFile()) {
+                if (file != null && file.isFile()) {
                     files.add(file.getName());
                 }
             }
         }
         return files;
     }
- }
+}
